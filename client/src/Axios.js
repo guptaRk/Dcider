a019af501de8b7cd6d2ad4b8ede596ad9420c717
subham <subham301477@gmail.com>
2019-03-31 17:37:09 +0530

    fix: proxy removed

--- client/src/Axios.js
+++ client/src/Axios.js
@@ -1,7 +1,11 @@
 const axios = require('axios');
 
+const defaultString = (process.env.NODE_ENV === 'production')
+  ? 'https://dcider.herokuapp.com/api'
+  : 'http://localhost:3000/api';
+
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/api/'
+  baseURL: defaultString
 });
 
 export default instance;
